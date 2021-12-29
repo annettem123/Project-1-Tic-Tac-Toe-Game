b083772693fe9238fd677f0396a4e9994ad8478e
Annette Mavec <admin@ADMINs-MacBook-Pro.local>
2021-12-29 02:35:53 -0600

    added ReadMe and draft of function checkTie

--- README.md
+++ README.md
@@ -1,20 +1,49 @@
-Next steps
-Stand Ups:
+Tecnologies Used:
+
+https://wireframe.cc/
+github.com
+terminal/CLI
+Git
+Visual Studio
+Dev Tools
+HTML
+CSS
+Javascript
+
+Links: wireframe and user stories
+
+Planning Steps and story about development process and problem-solving strategy:
+
+Prepare for Stand Ups:
+
 1. What have you done?
 created a new repo in git hub
 forked and cloned code from new repo in git hub to Projects Directory
 created new files index.html, styles.css, script.js, and README.md inside of Projects Directory
-review my past notes 
-2. What will you do today?
-in HTML, create 9 divs
-in JS, work on logic 
+review my past notes and documentation from lessons 
 
-review digital games (player 1 player 2)
-review lessons of code to determine which are applicable
+2. What will you do today?
+
 create wireframe
 create user stories
+write some pseudocode
+in HTML, create 9 divs
+in JS, work on logic 
+review digital games (player 1 player 2)
+review lessons of code to determine which are applicable
 add some styling and link styles.css to index.html
 add some JS and link script.js to index.html
-write some pseudocode
+
 3. What barriers might you encounter today?
-code related
+
+using the right code and syntax
+
+if stuck: 1) google; 2) post ? on slack or ask another student; 3) create an issue and ask instructor
+
+4. How close to MVP (Minimum Viable Product)?
+
+5. Unsolved Problems which would be fixed in future iterations:
+
+    Show a draw if neither player wins
+
+
--- script.js
+++ script.js
@@ -1,45 +1,108 @@
-let playerOneTurn = true
+let playerOneTurn = true // Data variable to track is "Whose turn is it?" which will be done using playerOneTurn and is to Boolean using true. 
 let gameBoard = [ 
     "0", "1", "2", 
     "3", "4", "5", 
-    "6", "7", "8",]
+    "6", "7", "8",]  // gameBoard is an array of elements because we are using multiple, similar items; namely, letters as strings:  "X"s and "O"s. An event listener cannot be added to an array. So, first, we move through the array, then we add an event listener to it.
 
-console.log(playerOneTurn)
+// console.log(playerOneTurn) // used this console.log to check if playerOneTurn worked. If it prints true, then it works.
 
 let square = document.querySelectorAll('.square');
 console.log(square)
 
 square.forEach(box => { 
-    box.addEventListener("click", handleClickEvent);
+    box.addEventListener("click", handleClickEvent);//An event Listener is set up such that if any of the boxes (or squares/spaces) are clicked, the handleClickEvent function takes place. The first argument is "click", and the second argument is the callback function for our event handler.
 })
+    
+function handleClickEvent (event){ //Defines function handleClickEvent. This holds the logic of what happens when one clicks on a square.  We want playerOneTurn to display X and switch turns and display O when it is not playOneTurn.
+    console.log(event.target); //Parameter "event.target" is used to get the element that we are clicking into. When one clicks on a square, the element will print. One sees the individual element, i.e., id="8" or id="0", etc.  Now we can actually grab the element to add something to it or show something into it.
+  if(playerOneTurn) {          //Having variable name in () and not having anything to compare it to is okay because it is a Boolean, and therefore, it is automatically checking to see if it is true (implicitly, it automatically checks to see if it is true. It is ok if one does put if playerOneTurn = true, but not necessary).
+    //   event.target.innerText = ""
+      event.target.innerText===''//playerOneTurn may only click on squares that are empty.
+      event.target.innerText="X" //What is happening: you click on a square,
+      gameBoard[event.target.id] = "X" //and it is asking, "Is it playerOneTurn"? If it is, we are going to put an "X" in that square that playerOneTurn clicked on.
+      playerOneTurn=!playerOneTurn //Here, is where we change the turn from playerOneTurn to be player two's Turn by making boolean false by using =! 
+      
+    }
+    else{
+        event.target.innerText===''//Player Two may only click on a square that is blank.
+        event.target.innerText="O"//When the turn changes from playerOneTurn to player Two's turn, 
+        gameBoard[event.target.id] = "O"//and it knows that since it is not playerOneTurn", it therefore must be player Two's Turn and once player Two clicks on a square, it will put an "O" into that square.
+        playerOneTurn=!playerOneTurn //Then, it will evaluate whether playerOneTurn or opponent wins by going through the function checkWinner below.
+    }
 
-
-function handleClickEvent (event){
-    console.log(event.target);
-  if(playerOneTurn) {
-      event.target.innerText="X"
-      gameBoard[event.target.id] = "X"
-      playerOneTurn=!playerOneTurn
-  }
-   else{
-       event.target.innerText="O"
-       gameBoard[event.target.id] = "O"
-       playerOneTurn=!playerOneTurn 
-   }
    checkWinner();
 //    console.log(gameBoard);
 }
+ 
 function checkWinner(){
-    if(gameBoard[0] === "X" && gameBoard[1] === "X" && gameBoard[2] === "X") {
+    if(gameBoard[0] === "X" && gameBoard[1] === "X" && gameBoard[2] === "X") { //checks if Player One selected 3 squares in a row in Row 1
         console.log("Player One Wins!");
     }
-
+    if(gameBoard[3] === "X" && gameBoard[4] === "X" && gameBoard[5] === "X") { //checks if Player One selected 3 squares in a row in Row 2
+        console.log("Player One Wins!");
+    }
+    if(gameBoard[6] === "X" && gameBoard[7] === "X" && gameBoard[8] === "X") { //checks if Player One selected 3 squares in a row in Row 3
+        console.log("Player One Wins!");
+    } 
+    if(gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard[6] === "X") { //checks if Player One selected 3 squares in a row in Column 1
+        console.log("Player One Wins!");
+    }
+    if(gameBoard[1] === "X" && gameBoard[4] === "X" && gameBoard[7] === "X") { //checks if Player One selected 3 squares in a row in Column 2
+        console.log("Player One Wins!");
+    }
+    if(gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard[8] === "X") { //checks if Player One selected 3 squares in a row in Column 3
+        console.log("Player One Wins!");
+    }
+    if(gameBoard[6] === "X" && gameBoard[4] === "X" && gameBoard[2] === "X") { //checks if Player One selected 3 squares in a diagonal row (Colum 1, Row 3 to Column 3, Row 1)
+        console.log("Player One Wins!");
+    }
+    if(gameBoard[0] === "X" && gameBoard[4] === "X" && gameBoard[8] === "X") { //checks if Player One selected 3 squares in a diagonal row (Colum 1, Row 1 to Column 3, Row 3)
+        console.log("Player One Wins!");
+    }
+    if(gameBoard[0] === "O" && gameBoard[1] === "O" && gameBoard[2] === "O") { //checks if Player Two selected 3 squares in a row in Row 1
+        console.log("Player Two Wins!");
+    }
+    if(gameBoard[3] === "O" && gameBoard[4] === "O" && gameBoard[5] === "O") { //checks if Player Two selected 3 squares in a row in Row 2
+        console.log("Player Two Wins!");
+    }
+    if(gameBoard[3] === "O" && gameBoard[4] === "O" && gameBoard[5] === "O") { //checks if Player Two selected 3 squares in a row in Row 3
+        if(gameBoard[6] === "O" && gameBoard[7] === "O" && gameBoard[8] === "O") {
+        console.log("Player Two Wins!");
+    }
+    if(gameBoard[0] === "O" && gameBoard[3] === "O" && gameBoard[6] === "O") { //checks if Player Two selected 3 squares in a row in Column 1
+        console.log("Player Two Wins!");
+    }
+    if(gameBoard[1] === "O" && gameBoard[4] === "O" && gameBoard[7] === "O") { //checks if Player Two selected 3 squares in a row in Column 1
+        console.log("Player Two Wins!");
+    }
+    if(gameBoard[2] === "O" && gameBoard[5] === "O" && gameBoard[8] === "O") { //checks if Player Two selected 3 squares in a row in Column 1
+        console.log("Player Two Wins!");
+    }
+    if(gameBoard[6] === "O" && gameBoard[4] === "O" && gameBoard[2] === "O") { //checks if Player Two selected 3 squares in a diagonal row (Colum 1, Row 3 to Column 3, Row 1)
+        console.log("Player Two Wins!");
+    }
+    if(gameBoard[0] === "O" && gameBoard[4] === "O" && gameBoard[8] === "O") { //checks if Player Two selected 3 squares in a diagonal row (Colum 1, Row 1 to Column 3, Row 3)
+        console.log("Player Two Wins!");
+    }
+    }
 }
 
-// square.forEach(element => console.log( `clicked ${element}`))
-//This is the event listener
-// square.addEventListener("click", handleClickEvent);
-// // variable holding the `querySelectorAll` for the squares
+// function checkTie()
+// {
+// 	if(gameBoard().length == 0) // EVERY SQUARE FILLED UP *NO WINNER*
+// 	{
+// 		for (var i = 0; i < gameBoard.length; i++)
+// 		{
+			
+// 			cells[i].removeEventListener('click', playerOneTurn, false);
 
-// forEach loop on the vaßriable 
-//   Add the event listener inside the loop
+// 			stopwatch.running = false;
+// 			stopwatch.time = null;
+// 		}
+
+// 		declareWinner("Game is a Tie !! ")
+// 		return true;
+
+// 		}
+// 		return false;
+// }
